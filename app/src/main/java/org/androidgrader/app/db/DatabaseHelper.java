9ed507a3caa7d70a4e810cd1631e03c6bffb1960
Andrew Smith <smith.andrew.l@gmail.com>
2015-11-28 20:28:59 -0600

    Added ormlite as a dependency.

---
+++ app/src/main/java/org/androidgrader/app/db/DatabaseHelper.java
@@ -0,0 +1,40 @@
+package org.androidgrader.app.db;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+
+
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+
+    public static final String DATABASE_NAME = "";
+    public static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+        try {
+            //TableUtils.createTable(connectionSource, fdas.class);
+        } catch(SQLException e) {
+
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        try {
+
+        } catch(SQLException e) {
+            // TableUtils.dropTable(connectionSource, fdas.class, true);
+            onCreate(database, connectionSource);
+        }
+    }
+
+
+}
+
