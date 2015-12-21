991ac2d757f18dfe322a3937bdc35f15a7db34bd
Andrew Smith <smith.andrew.l@gmail.com>
2015-12-20 20:32:41 -0600

    Corrected version in DatabaseHelper

--- app/src/main/java/org/androidgrader/app/db/DatabaseHelper.java
+++ app/src/main/java/org/androidgrader/app/db/DatabaseHelper.java
@@ -11,7 +11,7 @@ import com.j256.ormlite.table.TableUtils;
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 
     public static final String DATABASE_NAME = "androidgrader.db";
-    public static final int DATABASE_VERSION = 3;
+    public static final int DATABASE_VERSION = 1;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
